f113646591cc386c2a7c52cf7decae46eb69e3bb
Jens Hoepken <jens@sourceflux.de>
2016-04-22 13:16:41 +0200

    Add preliminary database fields

--- patient/models.py
+++ patient/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Seizure(models.Model):
+
+    time = models.DateTimeField('seizure time')
+    duration = models.IntegerField('duration in seconds')
+    description = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.description
+
