e0df3c90defed6f41f4b35feb21a52705e206048
Jens Hoepken <jens@sourceflux.de>
2016-04-23 12:06:33 +0200

    Add template for top nav bar

--- raspilepsy/settings.py
+++ raspilepsy/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'raspilepsy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'patient','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
