9433a7ceda2edf6c004063b164a0196b45b484a1
Jens Hoepken <jens@sourceflux.de>
2016-04-27 11:13:29 +0200

    Add SeizureManager

--- patient/models.py
+++ patient/models.py
@@ -4,12 +4,21 @@ from django.db import models
 
 # Create your models here.
 
+class SeizureManager(models.Manager):
+    def getDaysWithSeizures(self):
+    def getOldestSeizure(self):
+        """
+        Returns the oldest seizure that is stored in the database
+        """
+        return self.all().order_by('time')[0]
+    
+
 class Seizure(models.Model):
 
     time = models.DateTimeField('seizure time', auto_now_add=True)
     duration = models.IntegerField('duration in seconds')
     description = models.TextField(max_length=5000)
+    objects = SeizureManager()
 
     def __str__(self):
         return self.description
-
