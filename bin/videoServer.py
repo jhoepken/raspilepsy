b137c8675717537c931c83bfcaaf35677c402ca7
Jens Hoepken <jens@sourceflux.de>
2016-05-02 19:08:22 +0200

    Add live video feed of piCam :)

--- bin/videoServer.py
+++ bin/videoServer.py
@@ -4,25 +4,19 @@ import imutils
 import time
 import argparse
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
-def hasMotion(frame):
 
-    return True
+def initPiCamera():
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    
+    time.sleep(0.1)
 
-def annotate(frame, status):
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(status), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-    return frame
-
-def displayVideoFeeds():
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-
+    return (camera, rawCapture)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="Path to the video file")
@@ -31,61 +25,21 @@ args = vars(ap.parse_args())
 
 # Read from live from camera
 if args.get("video", None) is None:
-    pass
+    camera, rawCapture = initPiCamera()
 
-# Read from file
-else:
-    camera = cv2.VideoCapture(args["video"])
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
-# initialize the first frame in the video stream
-firstFrame = None
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
 
-while True:
-    (grabbed, frame) = camera.read()
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
-    status = "No seizure"
-
-    # If the frame cannot be grabbed, the end of the video has been reached.
-    if not grabbed:
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
         break
-
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
- 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        status = "Seizure"
-
-        frame = annotate(frame, status)
-
-        displayVideoFeeds()
- 
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
