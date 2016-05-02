77d7172107286936add202498f523ae6539f2d46
Jens Hoepken <jens@sourceflux.de>
2016-05-02 22:01:20 +0200

    Clean up code, fix parameters

--- bin/videoServer.py
+++ bin/videoServer.py
@@ -28,8 +28,6 @@ def highlightMotion(frame, avg):
     # if the average frame is None, initialize it
     if avg is None:
         avg = gray.copy().astype("float")
-        # rawCapture.truncate(0)
-        # continue
  
     # accumulate the weighted average between the current frame and
     # previous frames, then compute the difference between the current
@@ -37,17 +35,13 @@ def highlightMotion(frame, avg):
     cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
 
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
     (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.imshow("gray", gray)
-    cv2.imshow("thres", thresh)
-    cv2.imshow("frameDelta", frameDelta)
 
     for c in cnts:
         if cv2.contourArea(c) < args["min_area"]:
@@ -56,10 +50,10 @@ def highlightMotion(frame, avg):
         # compute the bounding box for the contour, draw it on the frame,
         # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
         text = "Seizure"
 
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+    cv2.putText(frame, "Status: {}".format(text), (10, 20),
     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     return (frame, avg)
@@ -68,6 +62,7 @@ def annotateTime(frame):
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
     (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
     return frame
+
 
 def hasMotion(frame):
 
@@ -89,8 +84,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # and occupied/unoccupied text
     image = frame.array
 
-    if hasMotion(image):
-        (image, firstFrame) = highlightMotion(image, firstFrame)
+    (image, firstFrame, hasMotion) = highlightMotion(image, firstFrame)
 
     image = annotateTime(image)
     # show the frame
