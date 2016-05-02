5c9c70ac5b92ac82969ea7b363ddfe99608b2d79
Jens Hoepken <jens@sourceflux.de>
2016-05-02 13:51:45 +0200

    Add skeleton to distinguish between live and file reading

--- videoServer.py
+++ videoServer.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 
+import argparse
 import cv2
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="Path to the video file")
+args = vars(ap.parse_args())
+
+# Read from live from camera
+if args.get("video", None) is None:
+    pass
+
+# Read from file
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+# initialize the first frame in the video stream
+firstFrame = None
