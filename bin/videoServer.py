6c689dc640a46840da285a9a6480c1809a4e1442
Jens Hoepken <jens@sourceflux.de>
2016-05-02 21:17:11 +0200

    Add function to draw boxes around motion, doesn't work, yet.

--- bin/videoServer.py
+++ bin/videoServer.py
@@ -18,10 +18,45 @@ def initPiCamera():
 
     return (camera, rawCapture)
 
-def highlightMotion():
-    pass
+def highlightMotion(frame, firstFrame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-def hasMotion():
+    text = "No Seizure"
+    
+    if firstFrame is None:
+        firstFrame = gray
+
+    # compute the absolute difference between the current frame and
+    # first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+        # if cv2.contourArea(c) < args["min_area"]:
+            # continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Seizure"
+
+    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+    return frame
+
+
+def hasMotion(frame):
 
     return True
 
@@ -39,6 +74,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
+
+    firstFrame = None
+
+    if hasMotion(image):
+        image = highlightMotion(image, firstFrame)
 
     # show the frame
     cv2.imshow("Frame", image)
