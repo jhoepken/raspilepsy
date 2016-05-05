2409059b21dc619ef84e2d637de123af3e36626a
Jens Hoepken <jens@sourceflux.de>
2016-05-05 21:33:25 +0200

    Change the frame width for the analyzed froame to 300px

--- bin/videoServer.py
+++ bin/videoServer.py
@@ -173,7 +173,7 @@ def highlightMotion(frame, avg, lastMotion):
     """
     global args
 
-    smallFrame = imutils.resize(frame, width=400)
+    smallFrame = imutils.resize(frame, width=300)
     gray = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
