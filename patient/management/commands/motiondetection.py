5cc62598a82a4954c5044eda7d78109d9362eaa0
Jens Hoepken <jens@sourceflux.de>
2016-05-09 18:09:26 +0200

    Add help tag

--- patient/management/commands/motiondetection.py
+++ patient/management/commands/motiondetection.py
@@ -2,7 +2,14 @@ from django.core.management import BaseCommand
 
 class Command(BaseCommand):
 
-    help = "You are on your own"
+    help = """
+    Server application for raspilepsy that takes care of continuous video
+    surveillance, seizure/motion detection and video recording. Videos of
+    seizures get stored automatically in database that is accessible via a
+    Django webapp.  This server application serves only for the purpose of data
+    aquisition and should be run via a cronjob. The users will not interact with
+    it.
+    """
     
     def handle(self, *args, **options):
 
